241ebe84cc208dc87d71fb52be5591439cbd2868
s0uzqw <luis.ribeiro2@estudante.ifms.edu.br>
2024-11-06 08:27:28 -0400

    Update CadastroProduto.tsx

--- src/CadastroProduto.tsx
+++ src/CadastroProduto.tsx
@@ -1,51 +1,63 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { FormEvent, useState, ChangeEvent } from "react";
 
 export default function CadastroProduto(){
     const [id,setId] = useState("")
+    const [descricao,setDescricao] = useState("")
+    const [nome,setNome] = useState("")
+    const [preco,setPreco] = useState("")
+    const [imagem,setImagem] = useState("")
+
     function handleForm(event:FormEvent){
         event.preventDefault();
         console.log("Tentei cadastrar produtos");
-        //Pegar os valores dos campos de input e colocar um objeto do tipo produto
+        //Pegar os valores dos campos de input e colocar em um objeto do tipo produto
         //Devo cadastrar no banco utilizando o BackEnd
-        //FETCH - > PRODUTO
+        //FETCH   - > PRODUTO
     }
-    function handleID(event:ChangeEvent:HTMLInputElement: any){
-        setId(event.target.value);
+    function handleId(event:ChangeEvent<HTMLInputElement>){
+        setId(event.target.value)
+    }
+    function handleDescricao(event:ChangeEvent<HTMLInputElement>){
+        setDescricao(event.target.value)
+    }
+    function handlePreco(event:ChangeEvent<HTMLInputElement>){
+        setPreco(event.target.value)
+    }
+    function handleNome(event:ChangeEvent<HTMLInputElement>){
+        setNome(event.target.value)
+    }
+    function handleImagem(event:ChangeEvent<HTMLInputElement>){
+        setImagem(event.target.value)
     }
 
     return(
         <>
-        <h1>Tela  de Cadastro de Produto</h1>
-        <form onSubmit={handleForm}>
-            <div>
-            <label htmlFor="id">ID</label>
-            <input type="text" name="id" onChange={handleID} />
-            </div>
-            
-            <div>
-            <label htmlFor="nome">nome</label>
-            <input type="text" name="nome" />
-            </div>
-            
-            <div>
-            <label htmlFor="descricao">descricao</label>
-            <input type="text" name="descricao" />
-            </div>
-            
-            <div>
-            <label htmlFor="preco">preço</label>
-            <input type="text" name="preco" />
-            </div>
-            
-            <div>
-            <label htmlFor="imagem">imagem</label>
-            <input type="text" name="imagem" />
-            </div>
-            
-            <div>
-            <input type="submit" value="Cadastrar" />
-            </div>
-        </form>
-    </>
+            <h1>Tela Cadastro Produtos</h1>
+            <form onSubmit={handleForm}>
+                <div>
+                    <label htmlFor="id">id</label>
+                    <input type="text" name="id" onChange={handleId} />
+                </div>
+                <div>
+                    <label htmlFor="nome">nome</label>
+                    <input type="text" name="nome" onChange={handleNome} />
+                </div>
+                <div>
+                    <label htmlFor="descricao">descricao</label>
+                    <input type="text" name="descricao" onChange={handleDescricao} />
+                </div>
+                <div>
+                    <label htmlFor="preco">preço</label>
+                    <input type="text" name="preco" onChange={handlePreco} />
+                </div>
+                <div>
+                    <label htmlFor="imagem">imagem</label>
+                    <input type="text" name="imagem" onChange={handleImagem}/>
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar"/>
+                </div>
+            </form>
+        </>
     )
 }
